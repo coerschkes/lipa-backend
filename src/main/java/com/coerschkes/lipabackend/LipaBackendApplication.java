d4d0d007d50f9652c5e8efaf19c86e7c17b32a3f
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-02-21 15:59:12 +0100

    chore: Add base project setup.

---
+++ src/main/java/com/coerschkes/lipabackend/LipaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coerschkes.lipabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LipaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LipaBackendApplication.class, args);
+	}
+
+}
